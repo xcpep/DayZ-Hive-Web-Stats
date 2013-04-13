7e4f4dc93df62e4e3f3b51452d051dd1206e748e
Daniel <sudoplays@gmail.com>
2013-04-13 17:57:24 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = ''; //MySQL host
+$user = ''; //MySQL user
+$pass = '';  //MySQL pass
+$db = '';	//MySQL Database
+
+
+$server_ip = '';	//Server IP
+$server_port = '';	//Server Port
+
+/**************ALL SETTINGS HERE ARE EITHER TRUE/FALSE**************/
+$perms['player_stats'] = true;      //Won't work if you change it, yet.
+$perms['player_signature'] = false; //Not implemented just yet.
