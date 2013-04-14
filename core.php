c203c692b9bcbffe8d2d8bf753b903148a1dd48e
Daniel <sudoplays@gmail.com>
2013-04-14 03:53:22 +0200

    Update core.php
    Enabled permissons.

--- core.php
+++ core.php
@@ -43,45 +43,51 @@ class Core {
 	// Game Tracker server status image.
 	public function server_status()
 	{
-		$url = '';
-		$url .= 'http://www.gametracker.com/server_info/';
-		$url .= $this->ip.':'.$this->port;
-		$url .= '/banner_560x95.png';
-		$img = '<div class="status"><img src="'.$url.'" align="center"/></div>';
-		echo $img;
+		if($this->perms['server_status'] == true)
+		{
+			$url = '';
+			$url .= 'http://www.gametracker.com/server_info/';
+			$url .= $this->ip.':'.$this->port;
+			$url .= '/banner_560x95.png';
+			$img = '<div class="status"><img src="'.$url.'" align="center"/></div>';
+			echo $img;
+		}
 	}
 
 	// Player stats page.
 	public function player_stats()
 	{
-		$select = mysqli_query($this->sql, "SELECT `id`,`unique_id`,`is_dead`,`survivor_kills`,`bandit_kills`,`zombie_kills`,`headshots`,`survival_time`,`last_updated` FROM `survivor`;");
-		echo('<table width="960px" id="stats" class="tablesorter" border="0" cellspacing="0" align="center">');
-		echo('<thead><tr>');
-		echo('	<th>ID</th>');
-		echo('	<th>Username</th>');
-		echo('	<th>Alive?</th>');
-		echo('	<th>Survivor Kills</th>');
-		echo('	<th>Bandit Kills</th>');
-		echo('	<th>Zombie Kills</th>');
-		echo('	<th>Headshots</th>');
-		echo('	<th>Survival Time</th>');
-		echo('	<th>Last Updated</th>');
-		echo('</tr></thead><tbody>');
-		while($r = mysqli_fetch_array($select, MYSQLI_ASSOC))
-		{ 
-			echo('<tr class="row">');
-			echo('	<td>'.$r['id'].'</td>');
-			echo('	<td>'.$this->get_username($r['unique_id']).'</td>');
-			echo('	<td>'.$this->dead($r['is_dead']).'</td>');
-			echo('	<td>'.$r['survivor_kills'].'</td>');
-			echo('	<td>'.$r['bandit_kills'].'</td>');
-			echo('	<td>'.$r['zombie_kills'].'</td>');
-			echo('	<td>'.$r['headshots'].'</td>');
-			echo('	<td>'.$this->time_format($r['survival_time']).'</td>');
-			echo('	<td>'.$r['last_updated'].'</td>');
-			echo('</tr>');
+		if($this->perms['player_stats'] == true)
+		{
+			$select = mysqli_query($this->sql, "SELECT `id`,`unique_id`,`is_dead`,`survivor_kills`,`bandit_kills`,`zombie_kills`,`headshots`,`survival_time`,`last_updated` FROM `survivor`;");
+			echo('<table width="960px" id="stats" class="tablesorter" border="0" cellspacing="0" align="center">');
+			echo('<thead><tr>');
+			echo('	<th>ID</th>');
+			echo('	<th>Username</th>');
+			echo('	<th>Alive?</th>');
+			echo('	<th>Survivor Kills</th>');
+			echo('	<th>Bandit Kills</th>');
+			echo('	<th>Zombie Kills</th>');
+			echo('	<th>Headshots</th>');
+			echo('	<th>Survival Time</th>');
+			echo('	<th>Last Updated</th>');
+			echo('</tr></thead><tbody>');
+			while($r = mysqli_fetch_array($select, MYSQLI_ASSOC))
+			{ 
+				echo('<tr class="row">');
+				echo('	<td>'.$r['id'].'</td>');
+				echo('	<td>'.$this->get_username($r['unique_id']).'</td>');
+				echo('	<td>'.$this->dead($r['is_dead']).'</td>');
+				echo('	<td>'.$r['survivor_kills'].'</td>');
+				echo('	<td>'.$r['bandit_kills'].'</td>');
+				echo('	<td>'.$r['zombie_kills'].'</td>');
+				echo('	<td>'.$r['headshots'].'</td>');
+				echo('	<td>'.$this->time_format($r['survival_time']).'</td>');
+				echo('	<td>'.$r['last_updated'].'</td>');
+				echo('</tr>');
+			}
+			echo('</tbody></table>');
 		}
-		echo('</tbody></table>');
 	}
 
 	public function dead($a)
