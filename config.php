25f943a77385cf6ba969a53b94127149a39028db
Daniel <sudoplays@gmail.com>
2013-04-14 03:54:09 +0200

    Update config.php
    Enabled permissions.

--- config.php
+++ config.php
@@ -9,5 +9,6 @@ $server_ip = '';	//Server IP
 $server_port = '';	//Server Port
 
 /**************ALL SETTINGS HERE ARE EITHER TRUE/FALSE**************/
-$perms['player_stats'] = true;      //Won't work if you change it, yet.
+$perms['player_stats'] = true;
 $perms['player_signature'] = false; //Not implemented just yet.
+$perms['server_status'] = true;
